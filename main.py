1a08e2a6d6342337c0090e63aab634389970d917
Quax2013 <robin.alex.ernst@gmail.com>
2025-03-24 18:24:43 +0100

    Added: Non-functional quarantine intervention

--- main.py
+++ main.py
@@ -9,16 +9,22 @@ def mask_wearing(sim):
     if sim.t == sim.day('2020-03-15'):
         sim['beta'] *= 0.5
 
+def quarantine(sim):
+    pass
+        
+
 if __name__ == "__main__":
     pars = {
-            'pop_size': 10e3, 
+            'pop_size': 50e3, 
             'start_day': '2020-02-01', 
             'n_days': 120
             }
     sim1 = cv.Sim(pars=pars, label='Default', analyzers=cv.age_histogram())
     sim2 = cv.Sim(pars=pars, label='Protect elderly', interventions=protect_elderly, analyzers=cv.age_histogram())
     sim3 = cv.Sim(pars=pars, label='Mask wearing', interventions=mask_wearing, analyzers=cv.age_histogram())
-    multiSim = cv.MultiSim([sim1, sim2, sim3])
+
+    sim5 = cv.Sim(pars=pars, label='Quarantine', interventions=quarantine, analyzers=cv.age_histogram())
+    multiSim = cv.MultiSim([sim1, sim2, sim3, sim5])
     multiSim.run().plot(to_plot=['cum_deaths', 'cum_infections'])
 
     analyzeSim = sim2
