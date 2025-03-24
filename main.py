422f611525cf20a98751c79c9274a71ab7693a50
Quax2013 <robin.alex.ernst@gmail.com>
2025-03-24 17:25:38 +0100

    Added: Covasim

---
+++ main.py
@@ -0,0 +1,26 @@
+import covasim as cv
+
+def protect_elderly(sim):
+    if sim.t == sim.day('2020-03-15'):
+        elderly = sim.people.age > 70
+        sim.people.rel_sus[elderly] = 0.1
+
+if __name__ == "__main__":
+    pars = {
+            'pop_size': 10e3, 
+            'start_day': '2020-02-01', 
+            'n_days': 120
+            }
+    sim1 = cv.Sim(pars=pars, label='Default', analyzers=cv.age_histogram())
+    sim2 = cv.Sim(pars=pars, label='Protect elderly', interventions=protect_elderly, analyzers=cv.age_histogram())
+    multiSim = cv.MultiSim([sim1, sim2])
+    multiSim.run().plot(to_plot=['cum_deaths', 'cum_infections'])
+
+    analyzeSim = sim2
+
+    analyzeRun = analyzeSim.run()
+    tt1 = analyzeRun.make_transtree()
+    tt1.plot_histograms()
+
+    hist1 = analyzeSim.get_analyzer()
+    hist1.plot()
